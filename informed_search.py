0ae53d643c55bee89254b8a2d3f1005edcc78b46
Ossama <ossedb@gmail.com>
2014-10-21 20:33:53 +0200

    Minor changes

--- informed_search.py
+++ informed_search.py
@@ -1,41 +1,47 @@
 import inspect
 
+
 class PriorityQueue(object):
-	def __init__(self, func):
-		self.func = func
-		self.queue = []
-	
-	def enqueue(self, val):
-		pass
-		
-	def dequeue(self):
-		return self.queue.pop(0)
+    def __init__(self, func):
+        self.func = func
+        self.queue = []
+
+    def enqueue(self, val):
+        pass
+
+    def dequeue(self):
+        return self.queue.pop(0)
+
 
 def greedy_best_first_search(G, h, s, f):
-	"""
-	G = {
-		'a': ['b', 'c'],
-		'b': ['a', 'c'],
-		'c': ['a']
-	}
-	"""
-	if not inspect.isfunction(h):
-		return False
-	
-	explored, frontier = [], [s]
-	
-	while frontier:
-		v = frontier.pop(0)
-		if v == f:
-			pass
-		else:
-			if v not in explored:
-				explored.append(v)
-				
-				for w in G[v]:
-					
-					pass
-					
-				frontier.extend([w for w in G[v] if w not in explored])
-	
-	return explored
+    """
+
+    :param G:
+    :param h:
+    :param s:
+    :param f:
+    :return:
+    """
+    if not inspect.isfunction(h):
+        return False
+
+    explored, frontier = [], [s]
+
+    while frontier:
+        v = frontier.pop(0)
+        if v == f:
+            pass
+        else:
+            if v not in explored:
+                explored.append(v)
+
+                for w in G[v]:
+                    pass
+
+                frontier.extend([w for w in G[v] if w not in explored])
+
+    return explored
+
+
+def a_star_search(G, h, s, f):
+    pass
--- uninformed_search.py
+++ uninformed_search.py
@@ -1,36 +1,40 @@
 
 def bfs(G, s, f):
-	"""
-	G = {
-		'a': ['b', 'c'],
-		'b': ['a', 'c'],
-		'c': ['a']
-	}
-	"""
-	explored, frontier = [], [s]
-	
-	while frontier:
-		v = frontier.pop(0)
-		if v == f:
-			pass
-		else:
-			explored.append(v)
-			frontier.extend([w for w in G[v] if w not in explored])
-	
-	return explored
-	
+    """
+
+    :param G:
+    :param s:
+    :param f:
+    :return:
+    """
+    explored, frontier = [], [s]
+
+    while frontier:
+        v = frontier.pop(0)
+        if v == f:
+            pass
+        else:
+            explored.append(v)
+            frontier.extend([w for w in G[v] if w not in explored])
+
+    return explored
 
 def dfs(G, s, f):
-	"""
-	"""
-	explored, frontier = [], [s]
-	
-	while frontier:
-		v = frontier.pop()
-		if v == f:
-			pass
-		else:
-			explored.append(v)
-			frontier.extend([w for w in G[v] if not in explored])
-		
-	return explored
+    """
+
+    :param G:
+    :param s:
+    :param f:
+    :return:
+    """
+    explored, frontier = [], [s]
+
+    while frontier:
+        v = frontier.pop()
+        if v == f:
+            pass
+        else:
+            explored.append(v)
+            frontier.extend([w for w in G[v] if w not in explored])
+
+    return explored
